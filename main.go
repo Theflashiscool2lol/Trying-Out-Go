59112ffdee91d4f28aaef8c3701316e3ccdc7cac
Theflashiscool2lol <theflashiscool200@gmail.com>
2022-03-06 08:30:48 -0800

    freeVbucks

--- main.go
+++ main.go
@@ -6,9 +6,19 @@ func main() {
 	var Name = "free vbucks"
 	const vbuckAmount = 10000
 	var vbucksremaining = 10000
-	fmt.Printf("want %v?", Name)
-	fmt.Println(" click here for", Name)
-	fmt.Printf("quickly as we only have %v vbucks left", vbucksremaining)
-	fmt.Printf("when we started we had %v vbucks so theyre selling out quick", vbuckAmount)
-	poop
+	fmt.Printf("want %v?\n", Name)
+	fmt.Printf("click here for %v\n", Name)
+	fmt.Printf("click quickly as we only have %v vbucks left\n", vbucksremaining)
+	fmt.Printf("when we started we had %v vbucks so theyre selling out quick\n", vbuckAmount)
+
+	var accountname string
+	var vbuckTheywant int
+	// ask user for the ammount of vbucks they want
+
+	accountname = "Theflashiscool2"
+	vbuckTheywant = 2000
+	fmt.Printf("You have selected %v vbucks to the account %v\n", vbuckTheywant, accountname)
+	fmt.Println("almost completed the vbuck giving proccess but now last step is to click this link")
+	fmt.Println("thanks for clicking the link ur ip house address discord token and fortnite account is now mine")
+
 }
