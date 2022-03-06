b4dffc4e377af7172010d263f989bf9b414d628f
Theflashiscool2lol <theflashiscool200@gmail.com>
2022-03-06 08:01:45 -0800

    te

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var Name = "free vbucks"
+	const vbuckAmount = 10000
+	var vbucksremaining = 10000
+	fmt.Printf("want %v?", Name)
+	fmt.Println(" click here for", Name)
+	fmt.Printf("quickly as we only have %v vbucks left", vbucksremaining)
+	fmt.Printf("when we started we had %v vbucks so theyre selling out quick", vbuckAmount)
+
+}poop
